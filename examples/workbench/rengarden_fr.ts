be22ab35e3da9dd76502902b1518d3041a0c6b07
Morwenn <morwenn29@hotmail.fr>
2015-01-18 13:40:12 +0100

    Added French translation for Ren Garden.

---
+++ examples/workbench/rengarden_fr.ts
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>watch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ren [人] Garden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Ren [人] Garden</source>
+        <translation>À propos de Ren [人] Garden</translation>
+    </message>
+    <message>
+        <source>The &lt;b&gt;Ren [人] Garden&lt;/b&gt; workbench integrates Rebol or Red evaluators into a Qt-based environment, by utilizing the Rencpp binding.
+
+Copyright © 2015 MetÆducation, GPL License
+
+Rebol, Red, and Qt are governed by the terms of their licenses.</source>
+        <translation>L'établi &lt;b&gt;Ren [人] Garden&lt;/b&gt; intègre les interpréteurs Rebol et Red dans un environnement Qt, en utilisant le binding Rencpp.
+
+Copyright © 2015 MetÆducation, GPL License
+
+Rebol, Red, et Qt sont régis par les modalités de leurs licences respectives.</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <source>Exit the application</source>
+        <translation>Quitter l'application</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>Cou&amp;per</translation>
+    </message>
+    <message>
+        <source>Cut the current selection&apos;s contents to the clipboard</source>
+        <translation>Coupe la sélection courante dans le presse-papiers</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Copier</translation>
+    </message>
+    <message>
+        <source>Copy the current selection&apos;s contents to the clipboard</source>
+        <translation>Copie la sélection courante dans le presse-papiers</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>&amp;Coller</translation>
+    </message>
+    <message>
+        <source>Paste the clipboard&apos;s contents into the current selection</source>
+        <translation>Colle le contenu du presse-papier dans la sélection courante</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;À propos</translation>
+    </message>
+    <message>
+        <source>Show the application&apos;s About box</source>
+        <translation>Affiche la rubrique À propos de l&apos;application</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>À propos de &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation>Affiche la rubrique À propos de la bibliothèque Qt</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <source>Switch layout direction</source>
+        <translation>Inverser la disposition des icônes</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Édition</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>Prêt</translation>
+    </message>
+</context>
+</TS>
